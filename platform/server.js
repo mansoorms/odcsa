06b1b35acc5517444c863557c4456aedc6f8877e
mansoorms <msmansoor@gmail.com>
2016-02-13 19:24:42 -0500

    Update server.js

--- platform/server.js
+++ platform/server.js
@@ -1,2 +1,14 @@
 <!-- this will serve as UI platform server-side scripting engine -->
 <!-- initially will use nodejs, socket.io, react -->
+
+var fs = require('fs');
+var http = require('http');
+var url = require('url') ;
+
+http.createServer(function (req, res) {
+  var queryObject = url.parse(req.url,true).query;
+  console.log(queryObject);
+
+  res.writeHead(200);
+  res.end('Feel free to add query parameters to the end of the url');
+}).listen(80);
