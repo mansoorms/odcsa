ad01d1451dabc0039c5af53fe2d2db36c4c5ca3e
Mohammed Rafique <smdrafique@gmail.com>
2016-03-24 01:14:13 -0400

    Create Date Updated

--- tmj-service/controllers/user.js
+++ tmj-service/controllers/user.js
@@ -33,10 +33,37 @@ var mailer = require('../helpers/email');
  });
 
  });
+ 
+ 
+ //get user active status..
+ router.get('/status/:au', function(req, res) {
+ console.log("Get Active/Inactive Users",req.query.active);
+	   var ipdata =req.query.active;
+	   var _connection = connection.get;
+	   console.log(ipdata);
+	   var query = _connection.query('SELECT * from USERS where active=?',[ipdata], function(err, rows,fields){
+		 data = [];
+           if(err) {
+               var data = [];
+			   data.push(err);
+               res.json(Response.error(data));
+           } else {
+					for (var i in rows) {
+						console.log(rows[i]);
+					  }
+               _.each(rows, function(d) {
+                   data.push(d);
+               });
+               res.json(Response.success(data));
+           }
+		   });
+ 
+ });
+ 
 
  //Get user deatils
  router.get('/:userID', function(req, res) {
-	   console.log("Get User Details");
+	   console.log("Get User Details",req.query.username);
 	   var ipdata =req.query.username;
 	   var _connection = connection.get;
 	   console.log(ipdata);
@@ -59,34 +86,114 @@ var mailer = require('../helpers/email');
 		
  });
 
- //get all active users..
- router.get('/:activeStatus', function(req, res) {
-
  
- });
  
  //get all active users..
- router.get('/:roles', function(req, res) {
-
+ router.get('/role/:rl', function(req, res) {
+	console.log("Get User Roles Details",req.query.role);
+	
+	 var ipdata =req.query.role;
+	   var _connection = connection.get;
+	   console.log(ipdata);
+	   var query = _connection.query('SELECT * from USERS where role=?',[ipdata], function(err, rows,fields){
+		 data = [];
+           if(err) {
+               var data = [];
+			   data.push(err);
+               res.json(Response.error(data));
+           } else {
+					for (var i in rows) {
+						console.log(rows[i]);
+					  }
+               _.each(rows, function(d) {
+                   data.push(d);
+               });
+               res.json(Response.success(data));
+           }
+		   });
  
  });
  //get all center details..
- router.get('/:centers', function(req, res) {
-
+ router.get('/center/:cn', function(req, res) {
+console.log("Get User Releated Center Details",req.query.center);
+	
+	 var ipdata =req.query.center;
+	   var _connection = connection.get;
+	   console.log(ipdata);
+	   var query = _connection.query('SELECT * from USERS where center=?',[ipdata], function(err, rows,fields){
+		 data = [];
+           if(err) {
+               var data = [];
+			   data.push(err);
+               res.json(Response.error(data));
+           } else {
+					for (var i in rows) {
+						console.log(rows[i]);
+					  }
+               _.each(rows, function(d) {
+                   data.push(d);
+               });
+               res.json(Response.success(data));
+           }
+		   });
  
  });
  
 
  //Delete a specific user
- router.delete('/:userID', function(req, res) {
+ router.put('/active/:ausr', function(req, res) {
+	 console.log("Active a User",req.query.username);
+	 var ipdata =req.query.username;
+	 var _connection = connection.get;
+	 console.log(ipdata);
+	 var query = _connection.query('UPDATE USERS SET active ="yes" WHERE active ="no" and username = ?', [ipdata], function(err, rows){
+		 if(err){
+                 console.log(err);
+           res.sendStatus(500);
+            }
+           res.sendStatus(200);
+     });
+
+ });
+ 
+ router.delete('/delete/:dlusr', function(req, res) {
+   console.log('Delete a Username : ',req.query.username);
+   var ipdata =req.query.username;
+   var _connection = connection.get;
+
+   var query = _connection.query("DELETE from USERS where username = ?",[ipdata], function(err, rows){
+	   
+           if(err){
+                console.log(err);
+				console.log('ClientConnectionReady Error: ' + err.message);
+                return next("Mysql error, check your query");
+           }
+		    console.log('Deleted: ' + rows.affectedRows + ' row');
+           
+          res.sendStatus(200);
+    });
 
  });
 
+
   //Create a new user
  router.post('/', function(req, res){
-	 console.log(req.body.username,req.body.password,req.body.firstName,req.body.lastName,req.body.email,req.body.image,
-	             req.body.active,req.body.lastLogin,req.body.lastActive,req.body.center,req.body.emailNotif,req.body.roles);
-   console.log("User Creation Service");
+	 console.log("User Creation Service");
+	 console.log(
+	             "\nusername:",req.body.username,
+	             "\npassword:",req.body.password,
+				 "\nfirstName:",req.body.firstName,
+				 "\nlastName:",req.body.lastName,
+				 "\nemail:",req.body.email,
+				 "\nImage:",req.body.image,
+	             "\nActive:",req.body.active,
+				 "\nLast Login:",req.body.lastLogin,
+				 "\nLast Active:",req.body.lastActive,
+				 "\nCenter:",req.body.center,
+				 "\nEmail Notification:",req.body.emailNotif,
+				 "\nRole:",req.body.role
+				 );
+   
    var data = {
     username:req.body.username,
 	password:req.body.password,
@@ -97,19 +204,23 @@ var mailer = require('../helpers/email');
 	active:req.body.active, 
 	center:req.body.center, 
 	emailNotif:req.body.emailNotif, 
-	roles:req.body.roles,
-	//createdDate:'CURRENT_DATE()'
+	role:req.body.role,
+	//createDate:'NOW()'
+	//createDate:req.body.createDate
 	
     };
 
     var _connection = connection.get;
 
-    var query = _connection.query("INSERT INTO USERS set ? ", data, function(err, rows){
+    var query = _connection.query("INSERT INTO USERS set createDate=NOW(), ? ", data, function(err, rows){
             if(err){
                  console.log(err);
-           res.sendStatus(500);
+				 console.log('ClientConnectionReady Error: ' + err.message);
+                 res.sendStatus(500);
             }
-           res.sendStatus(200);
+			console.log('Inserted: ' + rows.affectedRows + ' row');
+            console.log('Id inserted: ' + rows.insertId);
+            res.sendStatus(200);
      });
 
 
